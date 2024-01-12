81d6568554bc42aba18cdd59df4fcace8afe087f
mercurows <1335016790@qq.com>
2024-01-12 21:33:52 +0800

    TdoList

---
+++ vue_test/06_src_scoped样式/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <!-- 父级组件传数据到子组件 -->
+        <!-- age传入的字符串"18" -->
+        <!-- <Student name="李四" sex="女" age="18"/> -->
+        <!-- :age 相当于与v-bind，会执行双引号里面的表达式并将其结果传入 -->
+        <Student/>
+        <hr>
+        <School/>
+    </div>
+</template>
+
+<script>
+import Student from './components/Student.vue'
+import School from './components/School.vue'
+export default {
+    name: 'App',
+    components: { Student ,School}
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue_test/07_src_TodoList案例/components/MyFooter.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="todo-footer" v-show="total">
+        <label>
+			<!-- 第一种方式 -->
+            <!-- <input type="checkbox" :checked="isAll" @change="checkAll"> -->
+			<!-- 第二种方式 -->
+            <input type="checkbox" v-model="isAll" >
+        </label>
+        <span>
+            <span>已完成{{ doneTotal }}</span> /全部{{ total }}
+        </span>
+        <button class="btn btn-danger" @click="clearAll">清除已完成任务</button>
+    </div>
+</template>
+
+<script>
+export default {
+	name: 'MyFooter',
+	props: ['todos','checkAllTodo','clearAllTodo'],
+	computed: {
+		total() {
+			return this.todos.length
+		},
+		doneTotal() {
+			const x = this.todos.reduce((pre, current) => {
+				console.log('@', pre, current);
+				return pre + (current.done ? 1 : 0)
+			},0)
+			return x
+		},
+		// 对应第一种方式
+		// isAll() {
+		// 	return this.doneTotal == this.total && this.total>0
+		// }
+
+		// 对应第二种方式
+		isAll: {
+			get() {
+				return this.doneTotal == this.total && this.total>0
+			},
+			set(value) {
+				// console.log('@@',value);
+				this.checkAllTodo(value)
+			}
+		}
+
+	},
+	methods: {
+		// 对应第一种方式
+		// checkAll(e) {
+		// 	this.checkAllTodo(e.target.checked)
+		// }
+		clearAll() {
+			this.clearAllTodo()
+		}
+	},
+}
+</script>
+
+<style scoped>
+	/*footer*/
+	.todo-footer {
+		height: 40px;
+		line-height: 40px;
+		padding-left: 6px;
+		margin-top: 5px;
+	}
+
+	.todo-footer label {
+		display: inline-block;
+		margin-right: 20px;
+		cursor: pointer;
+	}
+
+	.todo-footer label input {
+		position: relative;
+		top: -1px;
+		vertical-align: middle;
+		margin-right: 5px;
+	}
+
+	.todo-footer button {
+		float: right;
+		margin-top: 5px;
+	}
+</style>
